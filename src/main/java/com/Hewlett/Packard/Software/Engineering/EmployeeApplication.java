da16779c1790dc9b3ca640be81a7956d5d4f8c51
Riddhesh Ranjan Rajwadkar <92072583+RiddheshRajwadkar@users.noreply.github.com>
2025-01-24 18:18:48 +0530

    Add files via upload

---
+++ src/main/java/com/Hewlett/Packard/Software/Engineering/EmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.Hewlett.Packard.Software.Engineering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeApplication.class, args);
+	}
+
+}
