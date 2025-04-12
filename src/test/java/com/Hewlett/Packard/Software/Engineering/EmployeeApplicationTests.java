0dbb3d52a209f40532caa59ac6acdf16777eafb0
RiddheshRajwadkar <riddheshrajwadkar49@gmail.com>
2025-04-12 12:09:56 +0530

    Completed writing the tests

---
+++ src/test/java/com/Hewlett/Packard/Software/Engineering/EmployeeApplicationTests.java
@@ -0,0 +1,15 @@
+package com.Hewlett.Packard.Software.Engineering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class EmployeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true); // just to check the Spring context loads successfully
+    }
+}
